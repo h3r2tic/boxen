be41f1cdb31ed3073d710ada5d432132a5ce5d1a
Tomasz Stachowiak <16522064+h3r2tic@users.noreply.github.com>
2010-01-21 04:01:28 +0000

    texture basics

---
+++ src/xf/gfx/Texture.d
@@ -0,0 +1,94 @@
+module xf.gfx.Texture;
+
+public {
+	import xf.gfx.TextureInternalFormat;
+}
+
+private {
+	import xf.gfx.Resource;
+	import xf.omg.core.LinearAlgebra;
+}
+
+
+typedef ResourceHandle TextureHandle;
+
+
+enum TextureType {
+	Texture1D = 0,
+	Texture2D,
+	Texture3D,
+	TextureCube,
+	TextureRectangle
+}
+
+
+enum TextureMinFilter {
+	Linear = 0,
+	Nearest,
+	NearestMipmapNearest,
+	NearestMipmapLinear,
+	LinearMipmapNearest,
+	LinearMipmapLinear
+}
+
+
+enum TextureMagFilter {
+	Linear = 0,
+	Nearest
+}
+
+
+enum TextureWrap {
+	NoWrap = 0,
+	Clamp,
+	ClampToEdge,
+	ClampToBorder
+}
+
+
+enum CubeMapFace {
+	PositiveX = 0,
+	NegativeX,
+	PositiveY,
+	NegativeY,
+	PositiveZ,
+	NegativeZ
+}
+
+
+struct TextureRequest {
+	TextureType			type			= TextureType.Texture2D;
+	InternalFormat		internalFormat	= InternalFormat.SRGB8_ALPHA8;
+	TextureMinFilter	minFilter		= TextureMinFilter.LinearMipmapLinear;
+	TextureMagFilter	magFilter		= TextureMagFilter.Linear;
+	TextureWrap			wrapS			= TextureWrap.NoWrap;
+	TextureWrap			wrapT			= TextureWrap.NoWrap;
+	TextureWrap			wrapR			= TextureWrap.NoWrap;
+	int					border			= 0;
+	vec4				borderColor		= {r: 0, g: 0, b: 0, a: 1};
+}
+
+
+interface ITextureMngr {
+	vec3i	getSize(TextureHandle handle);
+	size_t	getApiHandle(TextureHandle handle);
+}
+
+
+struct Texture {
+	alias TextureHandle Handle;
+	mixin MResource;
+
+
+	vec3i getSize() {
+		assert (_resHandle !is Handle.init);
+		assert (_resMngr !is null);
+		return (cast(ITextureMngr)_resMngr).getSize(_resHandle);
+	}
+
+	size_t getApiHandle() {
+		assert (_resHandle !is Handle.init);
+		assert (_resMngr !is null);
+		return (cast(ITextureMngr)_resMngr).getApiHandle(_resHandle);
+	}
+}
