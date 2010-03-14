a58705173d9ad5513834ea4fbf1e64d675ea528a
Tomasz Stachowiak <h3@h3.gd>
2010-03-14 00:06:33 +0000

    implemented a chunked xor deque

--- src/xf/mem/ChunkQueue.d
+++ src/xf/mem/ChunkQueue.d
@@ -10,7 +10,7 @@ private {
 
 
 
-struct RawChunkQueue {
+struct ScratchFIFO {
 	const size_t _pageSize = minDefaultPageSize;
 	
 	private alias _chunkQueueInternalAllocator
@@ -142,6 +142,44 @@ struct RawChunkQueue {
 	}
 
 
+	/+bool isEmpty() {
+		if (_head !is null) {
+			assert (_head.used > 0);
+		}
+		return _head is null;
+	}
+
+
+	struct IterateFruct {
+		RawChunkQueue	q;
+		size_t			itemSize;
+
+		int opApply(int delegate(ref void*) dg) {
+			QueueChunk* ch = q._head;
+			size_t itemSize = this.itemSize;
+			while (ch) {
+				void* ptr = ch.ptr();
+				size_t cntr = ch.used;
+				assert (0 == cntr % itemSize);
+				cntr /= itemSize;
+				
+				for (; cntr--; ptr += itemSize) {
+					void* x = ptr;
+					if (int r = dg(x)) {
+						return r;
+					}
+				}
+				ch = ch.next;
+			}
+
+			return 0;
+		}
+	}
+	IterateFruct iterateAssumingFixedSize(size_t itemSize) {
+		return IterateFruct(*this, itemSize);
+	}+/
+
+
 	size_t chunkCapacity() {
 		return _pageSize - _subAllocator.maxChunkOverhead - QueueChunk.sizeof;
 	}
@@ -150,6 +188,285 @@ struct RawChunkQueue {
 	QueueChunk*	_head;
 	QueueChunk*	_tail;
 	void*		_lastFreed;
+}
+
+
+
+/// No alignment guarantees
+struct RawChunkDeque {
+	const size_t _pageSize = minDefaultPageSize;
+	
+	private alias _chunkQueueInternalAllocator
+		_subAllocator;
+		
+	private alias chunkCache!(_pageSize - _subAllocator.maxChunkOverhead, _subAllocator)
+		_allocator;
+
+
+
+	static RawChunkDeque opCall(size_t itemSize) {
+		RawChunkDeque res;
+		res._itemSize = itemSize;
+		return res;
+	}
+		
+
+	private struct QueueChunk {
+		size_t	nextXorPrev;
+		size_t	capacity;		// bytes
+		size_t	head;			// beginning of first item, bytes
+		size_t	tail;			// one past end of last item, bytes
+
+		void* ptr() {
+			return (cast(void*)this) + QueueChunk.sizeof;
+		}
+	}
+	static assert (QueueChunk.sizeof == size_t.sizeof * 4);
+
+
+	private QueueChunk* allocNewChunk() {
+		memLog.trace("RawChunkDeque :: requesting a new chunk.");
+
+		auto raw = _allocator.alloc();
+		assert (raw !is null);
+		assert (raw.ptr !is null);
+		final chunk = cast(QueueChunk*)raw.ptr;
+		chunk.capacity = raw.size - QueueChunk.sizeof;
+
+		if (_itemSize > chunk.capacity) {
+			memError(
+				"Chunks too small ({}) to allocate {} bytes.",
+				chunk.capacity, _itemSize
+			);
+		}
+
+		return chunk;
+	}
+
+
+	void* pushBack() {
+		assert (_itemSize != 0);
+		
+		QueueChunk*	chunk = void;
+
+		if (_tail is null) {
+			_head = _tail = chunk = allocNewChunk();
+			chunk.head = chunk.tail = 0;
+			chunk.nextXorPrev = 0;
+		} else {
+			chunk = _tail;
+			
+			if (chunk.tail + _itemSize > chunk.capacity) {
+				chunk = allocNewChunk();
+				chunk.head = chunk.tail = 0;
+				_tail.nextXorPrev ^= cast(size_t)chunk;
+				chunk.nextXorPrev = cast(size_t)_tail;
+				_tail = chunk;
+			}
+		}
+
+		void* ptr = chunk.ptr() + chunk.tail;
+		chunk.tail += _itemSize;
+		return ptr;
+	}
+
+
+	void* pushFront() {
+		assert (_itemSize != 0);
+
+		QueueChunk*	chunk = void;
+
+		if (_head is null) {
+			_head = _tail = chunk = allocNewChunk();
+			chunk.head = chunk.tail = chunk.capacity;
+			chunk.nextXorPrev = 0;
+		} else {
+			chunk = _head;
+			
+			if (chunk.head < _itemSize) {
+				chunk = allocNewChunk();
+				chunk.head = chunk.tail = chunk.capacity;
+				_head.nextXorPrev ^= cast(size_t)chunk;
+				chunk.nextXorPrev = cast(size_t)_head;
+				_head = chunk;
+			}
+		}
+
+		chunk.head -= _itemSize;
+		return chunk.ptr() + chunk.head;
+	}
+
+
+	void* popFront() {
+		final ch = _head;
+		if (ch is null) {
+			memError("Queue underflow");
+		}
+		
+		void* res = ch.ptr + ch.head;
+
+		ch.head += _itemSize;
+
+		if (ch.head > ch.tail) {
+			memError("Queue underflow");
+		}
+
+		if (ch.head == ch.tail) {
+			final nhead = cast(QueueChunk*)ch.nextXorPrev;
+			_head = nhead;
+			
+			if (nhead is null) {
+				_tail = null;
+			} else {
+				nhead.nextXorPrev ^= cast(size_t)ch;
+			}
+
+			auto cache = &_allocator;
+			memLog.trace("RawChunkDeque :: releasing a chunk.");
+			Chunk.fromPtr(cast(void*)ch).dispose();
+			_head = nhead;
+		}
+
+		return res;
+	}
+
+
+	void* popBack() {
+		final ch = _tail;
+		if (ch is null || ch.tail < ch.head + _itemSize) {
+			memError("Queue underflow");
+		}
+		
+		ch.tail -= _itemSize;
+
+		void* res = ch.ptr + ch.tail;
+
+
+		if (ch.head == ch.tail) {
+			final ntail = cast(QueueChunk*)ch.nextXorPrev;
+			_tail = ntail;
+			
+			if (ntail is null) {
+				_head = null;
+			} else {
+				ntail.nextXorPrev ^= cast(size_t)ch;
+			}
+
+			auto cache = &_allocator;
+			Chunk.fromPtr(cast(void*)ch).dispose();
+			_tail = ntail;
+		}
+
+		return res;
+	}
+
+
+	void clear() {
+		size_t		prev	= 0;
+		QueueChunk*	cur		= _head;
+		
+		while (cur) {
+			final next = cast(QueueChunk*)(prev ^ cur.nextXorPrev);
+			scope (success)	{
+				prev = cast(size_t)cur;
+				cur = next;
+			}
+
+			memLog.trace("RawChunkDeque :: releasing a chunk.");
+			Chunk.fromPtr(cast(void*)cur).dispose();
+		}
+
+		_head = _tail = null;
+	}
+
+
+	void dispose() {
+		clear();
+	}
+
+
+	bool isEmpty() {
+		if (_head !is null) {
+			assert (_head.head != _head.tail);
+		}
+		return _head is null;
+	}
+
+
+	int opApply(int delegate(ref void*) dg) {
+		size_t		prev	= 0;
+		QueueChunk*	cur		= _head;
+		
+		while (cur) {
+			final next = cast(QueueChunk*)(prev ^ cur.nextXorPrev);
+			scope (success)	{
+				prev = cast(size_t)cur;
+				cur = next;
+			}
+
+			void* ptr = cur.ptr();
+			void* last = ptr + cur.tail;
+			void* it = null;
+
+			for (it = ptr + cur.head; it < last; it += _itemSize) {
+				void* meh = it;
+				if (int r = dg(meh)) {
+					return r;
+				}
+			}
+
+			assert (
+				it is last,
+				"Shit went bananas, someone changed _itemSize"
+				" during a RawChunkDeque's life?"
+			);
+		}
+
+		return 0;
+	}
+
+
+	int opApplyReverse(int delegate(ref void*) dg) {
+		size_t		prev	= 0;
+		QueueChunk*	cur		= _tail;
+		
+		while (cur) {
+			final next = cast(QueueChunk*)(prev ^ cur.nextXorPrev);
+			scope (success)	{
+				prev = cast(size_t)cur;
+				cur = next;
+			}
+
+			void* ptr = cur.ptr();
+			void* first = ptr + cur.head;
+			void* it = null;
+
+			assert (
+				0 == (cur.tail - cur.head) % _itemSize,
+				"Shit went bananas, someone changed _itemSize"
+				" during a RawChunkDeque's life?"
+			);
+
+			assert (cur.head <= cur.tail);
+
+			for (it = ptr + cur.tail; it != first; /+ nothing +/) {
+				it -= _itemSize;
+				void* meh = it;
+				if (int r = dg(meh)) {
+					return r;
+				}
+			}
+		}
+
+		return 0;
+	}
+
+
+	private {
+		QueueChunk*	_head;
+		QueueChunk*	_tail;
+		size_t		_itemSize;
+	}
 }
 
 
