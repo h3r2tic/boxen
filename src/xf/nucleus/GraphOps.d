87aab13e7692a1964196b14e82ff2791148fa2c1
Tomasz Stachowiak <h3@h3.gd>
2010-04-14 04:35:44 +0100

    humble beginnings of graph ops for nucleus

---
+++ src/xf/nucleus/GraphOps.d
@@ -0,0 +1,14 @@
+module xf.nucleus.GraphOps;
+
+private {
+	import xf.nucleus.Graph;
+}
+
+
+
+void findTopologicalOrder(Graph, GraphNodeId[]) {
+}
+
+
+void removeUnreachable(Graph, GraphNodeId[]) {
+}
