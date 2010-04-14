95c714c991317e32cf4a08622597172e0d60ae91
Tomasz Stachowiak <h3@h3.gd>
2010-04-14 09:23:54 +0100

    more of the graph impl - most code is in, but not tested - it's crash time :D

--- src/xf/nucleus/Graph.d
+++ src/xf/nucleus/Graph.d
@@ -4,7 +4,9 @@ private {
 	import xf.Common;
 	import xf.mem.FreeList;
 	import xf.mem.ChunkQueue;
-	import xf.omg.core.Misc : min;
+	import xf.mem.StackBuffer;
+	import xf.omg.core.Misc : min, max;
+	import xf.nucleus.Log : error = nucleusError;
 }
 
 
@@ -39,6 +41,13 @@ struct GraphNodeInfo {
 }
 
 
+struct DataFlow {
+	cstring from;
+	cstring to;
+	uword	userData;
+}
+
+
 
 template MSmallTempArray(T) {
 	enum { GrowBy = 8 }
@@ -60,6 +69,33 @@ template MSmallTempArray(T) {
 			items[0.._length] = _items[0.._length];
 			items[_length.._capacity] = T.init;
 		}
+	}
+
+	void remove(T* item) {
+		assert (_length > 0);
+		assert (item >= _items && item < _items + _length);
+		ushort idx = cast(ushort)(item - _items);
+		if (idx != _length-1) {
+			_items[idx] = _items[_length-1];
+		}
+		_items[_length-1] = T.init;
+	}
+
+	void removeMatching(bool delegate(T) pred) {
+		ushort dst = 0;
+		for (ushort src = 0; src < _length; ++src) {
+			if (!pred(_items[src])) {
+				if (dst != src) {
+					_items[dst] = _items[src];
+				}
+				++dst;
+			}
+		}
+
+		assert (dst <= _length);
+
+		_items[dst.._length] = T.init;
+		_length = dst;
 	}
 
 	void alloc(ushort num, ScratchFIFO mem) {
@@ -122,22 +158,28 @@ final class Graph {
 		_verifyNodeId(id);
 		_writeFlag(_presentFlags, id.id, false);
 		++_idReuseCounts[id.id];
+
+		_removeExternalConnectionsToAndFrom(id);
 		
 		// so their items can be reused
-		_incomingConnections[id.id]._length = 0;
-		_outgoingConnections[id.id]._length = 0;
+		_disposeConnectionList(&_incomingConnections[id.id]);
+		_disposeConnectionList(&_outgoingConnections[id.id]);
 	}
 	
 	void			removeNodes(bool delegate(GraphNodeId) pred) {
 		for (uword i = 0; i < _capacity; ++i) {
 			if (_readFlag(_presentFlags, i)) {
-				if (pred(GraphNodeId(cast(ushort)i, _idReuseCounts[i]))) {
+				final id = GraphNodeId(cast(ushort)i, _idReuseCounts[i]);
+				
+				if (pred(id)) {
 					_writeFlag(_presentFlags, i, false);
 					++_idReuseCounts[i];
 
+					_removeExternalConnectionsToAndFrom(id);
+
 					// so their items can be reused
-					_incomingConnections[i]._length = 0;
-					_outgoingConnections[i]._length = 0;
+					_disposeConnectionList(&_incomingConnections[i]);
+					_disposeConnectionList(&_outgoingConnections[i]);
 				}
 			}
 		}
@@ -180,30 +222,147 @@ final class Graph {
 
 	// ----
 
-	void			addDataFlow(GraphNodeId, cstring, GraphNodeId, cstring) {
-		assert (false, "TODO");
+	DataFlow*		addDataFlow(
+			GraphNodeId from, cstring fromPort,
+			GraphNodeId to, cstring toPort
+	) {
+		_verifyNodeId(from);
+		_verifyNodeId(to);
+		ConnectionList* outList = &_outgoingConnections[from.id];
+		ConnectionList* incList = &_incomingConnections[to.id];
+		Connection* con;
+		
+		foreach (item; outList.items) {
+			if (item.from.id is from.id) {
+				assert (item.from.reuseCnt is from.reuseCnt);
+				if (item.to.id is to.id) {
+					assert (item.to.reuseCnt is to.reuseCnt);
+					con = item;
+				}
+			}
+		}
+
+		if (con !is null) {
+			if (auto fl = con.containsFlow(fromPort, toPort)) {
+				return fl;
+			}
+		} else {
+			if (_connectionFreeList !is null) {
+				con = _connectionFreeList;
+				_connectionFreeList = con._freeListNext;
+				assert (0 == con._length);		// must be properly disposed
+			} else {
+				con = cast(Connection*)_mem.pushBack(Connection.sizeof);
+				*con = Connection.init;
+			}
+
+			con.from = from;
+			con.to = to;
+		}
+
+		con.pushBack(
+			DataFlow(
+				_allocString(fromPort),
+				_allocString(toPort)
+			),
+			_mem
+		);
+
+		return &con.items[con.length-1];
 	}
 	
-	void			removeDataFlow(GraphNodeId, cstring, GraphNodeId, cstring) {
-		assert (false, "TODO");
+	void			removeDataFlow(
+			GraphNodeId from, cstring fromPort,
+			GraphNodeId to, cstring toPort
+	) {
+		_verifyNodeId(from);
+		_verifyNodeId(to);
+		ConnectionList* outList = &_outgoingConnections[from.id];
+		ConnectionList* incList = &_incomingConnections[to.id];
+		Connection* con;
+		
+		foreach (item; outList.items) {
+			if (item.from.id is from.id) {
+				assert (item.from.reuseCnt is from.reuseCnt);
+				if (item.to.id is to.id) {
+					assert (item.to.reuseCnt is to.reuseCnt);
+					con = item;
+				}
+			}
+		}
+
+		if (con !is null) {
+			if (auto fl = con.containsFlow(fromPort, toPort)) {
+				con.remove(fl);
+
+				if (0 == con.length) {
+					_disconnectUnsafe(con);
+				}
+			} else {
+				error(
+					"Data flow from {}:{} to {}:{} doesn't exist:"
+					" valid node connection, no such data flow.",
+					from.id, fromPort, to.id, toPort
+				);
+			}
+		} else {
+			error(
+				"Data flow from {}:{} to {}:{} doesn't exist:"
+				" no such connection.",
+				from.id, fromPort, to.id, toPort
+			);
+		}
 	}
 	
-	void			removeDataFlow(GraphNodeId, GraphNodeId, bool delegate(cstring, cstring)) {
-		assert (false, "TODO");
+	void			removeDataFlow(
+			GraphNodeId from, GraphNodeId to,
+			bool delegate(cstring, cstring) pred
+	) {
+		_verifyNodeId(from);
+		_verifyNodeId(to);
+		ConnectionList* outList = &_outgoingConnections[from.id];
+		ConnectionList* incList = &_incomingConnections[to.id];
+		Connection* con;
+		
+		foreach (item; outList.items) {
+			if (item.from.id is from.id) {
+				assert (item.from.reuseCnt is from.reuseCnt);
+				if (item.to.id is to.id) {
+					assert (item.to.reuseCnt is to.reuseCnt);
+					con = item;
+				}
+			}
+		}
+
+		if (con !is null) {
+			con.removeMatching((DataFlow fl) {
+				return pred(fl.from, fl.to);
+			});
+
+			if (0 == con.length) {
+				_disconnectUnsafe(con);
+			}
+		} else {
+			error(
+				"Data flow from {} to {} doesn't exist:"
+				" no such connection.",
+				from.id, to.id
+			);
+		}
 	}
 	
-	FlowFruct		iterDataFlow(GraphNodeId, GraphNodeId) {
-		assert (false, "TODO");
+	FlowFruct		iterDataFlow(GraphNodeId from, GraphNodeId to) {
+		return FlowFruct(this, from, to);
 	}
 
 	// ----
 
-	OutgoingConnectionFruct	iterOutgoingConnections(GraphNodeId) {
-		assert (false, "TODO");
+	OutgoingConnectionFruct	iterOutgoingConnections(GraphNodeId id) {
+		return OutgoingConnectionFruct(this, id);
 	}
 	
-	IncomingConnectionFruct	iterIncomingConnections(GraphNodeId) {
-		assert (false, "TODO");
+	IncomingConnectionFruct	iterIncomingConnections(GraphNodeId id) {
+		return IncomingConnectionFruct(this, id);
 	}
 
 	// ----
@@ -229,8 +388,38 @@ final class Graph {
 	// ----
 
 	void			removeAllFlow(GraphNodeId from, GraphNodeId to) {
+		_verifyNodeId(from);
+		_verifyNodeId(to);
+
+		// remove auto flow
+		
 		_writeFlag(_autoFlowFlags, from.id, to.id, false);
-		assert (false, "TODO");
+
+		// remove data flow
+
+		ConnectionList* outList = &_outgoingConnections[from.id];
+		ConnectionList* incList = &_incomingConnections[to.id];
+		Connection* con;
+		
+		foreach (item; outList.items) {
+			if (item.from.id is from.id) {
+				assert (item.from.reuseCnt is from.reuseCnt);
+				if (item.to.id is to.id) {
+					assert (item.to.reuseCnt is to.reuseCnt);
+					con = item;
+				}
+			}
+		}
+
+		if (con !is null) {
+			_disconnectUnsafe(con);
+		} else {
+			error(
+				"Data flow from {} to {} doesn't exist:"
+				" no such connection.",
+				from.id, to.id
+			);
+		}
 	}
 
 	// ----
@@ -256,56 +445,129 @@ final class Graph {
 			_mem = ScratchFIFO.init;
 			_reallocateNodes(maxUsed+1);
 			oldStorage.clear();
+			_connectionFreeList = null;		// In old mem now, do not want.
 		}
 	}
 
 	/**
 	 * Deletes all nodes and connections.
 	 */
-	void			dispose() {
-		_mem.clear();
-		_presentFlags = null;
-		_idReuseCounts = null;
-		_nodeInfos = null;
-		_nodeLabels = null;
-		_autoFlowFlags = null;
-
-		_incomingConnections = null;
-		_outgoingConnections = null;
-		
-		_connectionFreeList = null;
-		_dataFlowFreeList = null;
-		
-		_numNodes = 0;
-		_capacity = 0;
+	void			reset() {
+		_dispose();
+		_mem.initialize();
 	}
 
 
 	private {
 		ScratchFIFO	_mem;
 
+		void _dispose() {
+			_mem.clear();
+			
+			_presentFlags = null;
+			_idReuseCounts = null;
+			_nodeInfos = null;
+			_nodeLabels = null;
+			_autoFlowFlags = null;
+
+			_incomingConnections = null;
+			_outgoingConnections = null;
+			
+			_connectionFreeList = null;
+			
+			_numNodes = 0;
+			_capacity = 0;
+		}
+
 		this() {
 			_mem.initialize();
 		}
 
 		~this() {
-			_mem.clear();
+			_dispose();
 		}
 
 		int _iterDataFlow(
-			GraphNodeId, GraphNodeId, int delegate(ref cstring, ref cstring)
-		);
+				GraphNodeId from, GraphNodeId to, int delegate(ref DataFlow) sink
+		) {
+			_verifyNodeId(from);
+			_verifyNodeId(to);
+			
+			ConnectionList* outList = &_outgoingConnections[from.id];
+			ConnectionList* incList = &_incomingConnections[to.id];
+			Connection* con;
+			
+			foreach (item; outList.items) {
+				if (item.from.id is from.id) {
+					assert (item.from.reuseCnt is from.reuseCnt);
+					if (item.to.id is to.id) {
+						assert (item.to.reuseCnt is to.reuseCnt);
+						con = item;
+					}
+				}
+			}
 
-		int _iterNodes(int delegate(ref GraphNodeId)) {
-			assert (false, "TODO");
+			if (con !is null) {
+				foreach (ref fl; con.items) {
+					cstring fb = fl.from;
+					cstring tb = fl.to;
+					int r = sink(fl);
+					assert (fb is fl.from, "Do not touch the strings :F");
+					assert (tb is fl.to, "Do not touch the strings :F");
+					if (r) return r;
+				}
+			} else {
+				error(
+					"Data flow from {} to {} doesn't exist:"
+					" no such connection.",
+					from.id, to.id
+				);
+			}
+
+			return 0;
 		}
 
-		int	_iterOutgoingConnections(GraphNodeId, int delegate(ref GraphNodeId)) {
-			assert (false, "TODO");
+		int _iterNodes(int delegate(ref GraphNodeId) sink) {
+			for (uword i = 0; i < _capacity; ++i) {
+				if (_readFlag(_presentFlags, i)) {
+					auto id = GraphNodeId(cast(ushort)i, _idReuseCounts[i]);
+					if (int r = sink(id)) {
+						return r;
+					}
+				}
+			}
+
+			return 0;
 		}
 
-		int	_iterIncomingConnections(GraphNodeId, int delegate(ref GraphNodeId)) {
-			assert (false, "TODO");
+		int	_iterOutgoingConnections(GraphNodeId from, int delegate(ref GraphNodeId) sink) {
+			_verifyNodeId(from);
+
+			ConnectionList* outList = &_outgoingConnections[from.id];
+			
+			foreach (item; outList.items) {
+				auto con = item.to;
+				if (int r = sink(con)) {
+					return r;
+				}
+			}
+
+			return 0;
+		}
+
+		int	_iterIncomingConnections(GraphNodeId to, int delegate(ref GraphNodeId) sink) {
+			_verifyNodeId(to);
+
+			ConnectionList* incList = &_incomingConnections[to.id];
+
+			foreach (item; incList.items) {
+				auto con = item.from;
+				if (int r = sink(con)) {
+					return r;
+				}
+			}
+
+			return 0;
 		}
 	}
 
@@ -319,24 +581,23 @@ final class Graph {
 				GraphNodeId	from;
 				GraphNodeId	to;
 
-				mixin MSmallTempArray!(DataFlow*);
+				mixin MSmallTempArray!(DataFlow);
 			}
 
+			DataFlow* containsFlow(cstring from, cstring to) {
+				foreach (ref it; items) {
+					if (it.from == from && it.to == to) {
+						return &it;
+					}
+				}
+
+				return null;
+			}
+
+			// occupies the same space as the 'from' GraphNodeId, thus safe to reuse
 			Connection*	_freeListNext;
 		}
 		
-		union DataFlow {
-			struct {
-				struct Item {
-					cstring from;
-					cstring to;
-				}
-
-				mixin MSmallTempArray!(Item);
-			}
-
-			DataFlow* _freeListNext;
-		}
 
 		uword*				_presentFlags;
 		ushort*				_idReuseCounts;
@@ -348,7 +609,6 @@ final class Graph {
 		ConnectionList*		_outgoingConnections;
 		
 		Connection*			_connectionFreeList;
-		DataFlow*			_dataFlowFreeList;
 		
 		uword			_numNodes;
 		uword			_capacity;
@@ -456,12 +716,8 @@ final class Graph {
 						dstCon.alloc(srcCon.length, _mem);
 						foreach (flowI, ref dstFlow; dstCon.items) {
 							final srcFlow = srcCon._items[flowI];
-							dstFlow.alloc(srcFlow.length, _mem);
-							foreach (dataI, ref dstData; dstFlow.items) {
-								final srcData = srcFlow._items[dataI];
-								dstData.from = _allocString(srcData.from);
-								dstData.to = _allocString(srcData.to);
-							}
+							dstFlow.from = _allocString(srcFlow.from);
+							dstFlow.to = _allocString(srcFlow.to);
 						}
 					}
 				}
@@ -480,8 +736,6 @@ final class Graph {
 			_autoFlowFlags = autoFlowFlags;
 			_incomingConnections = incomingConnections;
 			_outgoingConnections = outgoingConnections;
-			_connectionFreeList = null;
-			_dataFlowFreeList = null;
 		}
 
 		uword _getNewCapacity() {
@@ -492,6 +746,88 @@ final class Graph {
 			assert (id.id < _capacity);
 			assert (true == _readFlag(_presentFlags, id.id));
 			assert (id.reuseCnt == _idReuseCounts[id.id]);
+		}
+
+		void _disposeConnectionList(ConnectionList* list) {
+			foreach (ref item; list.items) {
+				_disposeConnection(item);
+			}
+			list._length = 0;
+		}
+
+		// Warning: Doesn't remove the connection from the incoming and outgoing lists
+		void _disposeConnection(Connection* con) {
+			con._length = 0;
+			con._freeListNext = _connectionFreeList;
+			_connectionFreeList = con;
+		}
+
+		void _removeExternalConnectionsToAndFrom(GraphNodeId id) {
+			_verifyNodeId(id);
+			
+			final inc = &_incomingConnections[id.id];
+			final oug = &_outgoingConnections[id.id];
+
+			scope stack = new StackBuffer;
+			final _conNodes = stack.allocArray!(ushort)(max(inc.length, oug.length));
+			uword numCon;
+
+			// remove all outgoing connections for nodes that are incoming to this node
+			{
+				numCon = 0;
+
+				foreach (con; inc.items) {
+					_conNodes[numCon++] = con.from.id;
+				}
+
+				foreach (n; _conNodes[0..numCon]) {
+					_outgoingConnections[n].removeMatching((Connection* c) {
+						bool res = c.to.id == id.id;
+						if (res) {
+							_disposeConnection(c);
+							return true;
+						} else {
+							return false;
+						}
+					});
+				}
+			}
+
+			// remove all incoming connections for nodes that are outgoing from this node
+			{
+				numCon = 0;
+
+				foreach (con; oug.items) {
+					_conNodes[numCon++] = con.to.id;
+				}
+
+				foreach (n; _conNodes[0..numCon]) {
+					_incomingConnections[n].removeMatching((Connection* c) {
+						bool res = c.from.id == id.id;
+						if (res) {
+							_disposeConnection(c);
+							return true;
+						} else {
+							return false;
+						}
+					});
+				}
+			}
+		}
+
+		// Unsafe because it may bork collections that are being iterated upon
+		void _disconnectUnsafe(Connection* con) {
+			ConnectionList* outList = &_outgoingConnections[con.from.id];
+			ConnectionList* incList = &_incomingConnections[con.to.id];
+
+			outList.removeMatching((Connection* c2) {
+				return con is c2;
+			});
+			incList.removeMatching((Connection* c2) {
+				return con is c2;
+			});
+
+			_disposeConnection(con);
 		}
 	}
 }
@@ -523,7 +859,7 @@ private {
 		
 		// The strings should be copied to stack-based buffers and only valid within
 		// the call that opApply will do to the sink delegate
-		int opApply(int delegate(ref cstring, ref cstring) sink) {
+		int opApply(int delegate(ref DataFlow) sink) {
 			return graph._iterDataFlow(fromNode, toNode, sink);
 		}
 	}
