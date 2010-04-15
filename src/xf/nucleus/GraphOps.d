b361322755d1fb06f921e7e9c8aea73cd5434e33
Tomasz Stachowiak <16522064+h3r2tic@users.noreply.github.com>
2010-04-15 04:00:04 +0100

    topological sorting for the nucleus graph

--- src/xf/nucleus/GraphOps.d
+++ src/xf/nucleus/GraphOps.d
@@ -2,11 +2,38 @@ module xf.nucleus.GraphOps;
 
 private {
 	import xf.nucleus.Graph;
+	import GraphUtils = xf.utils.Graph;
+	import xf.mem.StackBuffer;
 }
 
 
 
-void findTopologicalOrder(Graph, GraphNodeId[]) {
+void findTopologicalOrder(Graph graph, GraphNodeId[] result) {
+	scope stack = new StackBuffer;
+	int[] order = stack.allocArray!(int)(graph.numNodes);
+	
+	int numOrdered = GraphUtils.findTopologicalOrder(
+		// nodeIter
+		(void delegate(int) sink) {
+			foreach (n; graph.iterNodes) {
+				sink(n.id);
+			}
+		},
+		
+		// nodeSuccIter
+		(int n, void delegate(int) sink) {
+			foreach (n2; graph.iterOutgoingConnections(graph._getNodeId(n))) {
+				sink(n2.id);
+			}
+		},
+		
+		order
+	);
+
+	assert (numOrdered == graph.numNodes);
+	foreach (i, n; order) {
+		result[i] = graph._getNodeId(n);
+	}
 }
 
 
--- src/xf/test/unit/NucleusGraph/dotAndView.bat
+++ src/xf/test/unit/NucleusGraph/dotAndView.bat
@@ -1,2 +1,3 @@
+@echo off
 if exist %1 cat %1 | dot -Tpng -o %1.png -Gsize="28,28" -Gsep=.05
 if exist %1 %1.png
